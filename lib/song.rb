e9c74d992bd86ffb818cd0796ddc137ee90d404f
Emily Asch <e.asch@me.com>
2018-11-26 02:54:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,15 @@ class Song
   attr_accessor :name, :artist, :genre  
   
   @@all = []
-  @@count = 0 
+  
   @@genres = []
   
   def initialize(name)
     @name = name
     @artist = artist 
     @genre = genre 
+    @@count = 0 
+    
     
     @@all << self 
     
