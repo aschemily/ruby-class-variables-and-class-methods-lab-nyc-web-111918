4c0c539a7a0580f14ed39a42ba345eeed88f4334
Emily Asch <e.asch@me.com>
2018-11-26 02:44:28 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre  
+  
+  @@all 
+  
+  def initialize(name)
+    @name = name 
+  end
+  
+  def
+  
+  def self.all 
+    @@all 
+  end 
+  
+  
+end #end of class song 
